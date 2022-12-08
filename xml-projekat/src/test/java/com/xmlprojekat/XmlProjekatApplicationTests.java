357ce99acb8e79e3c5a087e902c7ebf10343018d
Tanita Mošić <tanitamosic@gmail.com>
2022-12-08 21:42:47 +0100

    Inicijalizovan Spring Boot projekat

---
+++ xml-projekat/src/test/java/com/xmlprojekat/XmlProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmlprojekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
