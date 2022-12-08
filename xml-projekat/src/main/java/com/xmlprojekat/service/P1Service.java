f9b851eaf22a447c43fd3d0ee28c5fc557564e21
Mihajlo Djordjevic <mihajlodjordjevic931@gmail.com>
2022-12-09 00:08:17 +0100

    Added service for handling P1 documents.

---
+++ xml-projekat/src/main/java/com/xmlprojekat/service/P1Service.java
@@ -0,0 +1,44 @@
+package com.xmlprojekat.service;
+
+import com.xmlprojekat.dom.DOMParser;
+import com.xmlprojekat.dto.XMLDto;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.util.Random;
+
+
+@Service
+public class P1Service extends XMLService {
+
+    private final DOMParser domParser;
+
+    public P1Service(DOMParser domParser) {
+        this.domParser = domParser;
+    }
+
+    public String applyZavod(XMLDto dto) throws IOException, SAXException, ParserConfigurationException, TransformerException {
+        Document document = domParser.buildDocumentFromText(dto.getText());
+        Element zavod = (Element) document.getElementsByTagName("forma_za_zavod").item(0);
+
+        Element brojPrijave = (Element) zavod.getElementsByTagName("broj_prijave").item(0);
+        brojPrijave.setTextContent(generateApplicationId());
+
+        Element datum = (Element) zavod.getElementsByTagName("datum_podnosenja").item(0);
+        setDateInDocument(datum);
+
+        return domParser.getDocumentAsString(document);
+    }
+
+    @Override
+    protected String generateApplicationId() {
+        Random r = new Random();
+        String id = String.valueOf(r.nextInt(80000) + 10000);
+        return "П " + id;
+    }
+}
