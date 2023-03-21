205d008191b926fc1a38dcb5d484d21e299f0c03
Mihajlo Djordjevic <mihajlodjordjevic931@gmail.com>
2023-03-21 15:37:17 +0100

    Added base backend project structure.

---
+++ backend/EntryService/src/main/java/xml/entryService/main/EntryServiceApplication.java
@@ -0,0 +1,13 @@
+package xml.entryService.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntryServiceApplication.class, args);
+	}
+
+}
