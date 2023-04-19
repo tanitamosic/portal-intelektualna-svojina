44bef7bf1ad88b654c420d43fe0aa5a929b2c54c
Petar Kupusarević <petar.kupusarevic@gmail.com>
2023-04-19 22:05:39 +0200

    Finished Login and Register

--- backend/EntryService/src/main/java/xml/entryService/main/service/UserService.java
+++ backend/EntryService/src/main/java/xml/entryService/main/service/UserService.java
@@ -1,8 +1,33 @@
 package xml.entryService.main.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import xml.entryService.main.model.User;
+import xml.entryService.main.repository.UserRepository;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User Authenticate(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
+    }
+
+    public User Register(String email, String password, String name, String surname, String role) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setName(name);
+        user.setSurname(surname);
+        user.setRole(role);
+        return userRepository.save(user);
+    }
+
 }
