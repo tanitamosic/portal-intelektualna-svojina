d3aff3cf62378382d43f0046973da4ced662ca7a
Petar Kupusarević <petar.kupusarevic@gmail.com>
2023-04-19 22:05:27 +0200

    Created Models and Initialized Database

---
+++ backend/EntryService/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into users ( email, password, name, surname,role) values
+('user1mail@google.com','password1','Petar','Petrovic','USER');
+
+insert into users ( email, password, name, surname,role) values
+('user2mail@google.com','password2','Mihajlo','Mihajlovic','USER');
+
+insert into users ( email, password, name, surname,role) values
+('user3mail@google.com','password3','Tanita','Tanitovic','USER');
+
+insert into users ( email, password, name, surname,role) values
+('sluzbenikmail@google.com','passowrd','Sluzbenik','McSluzbenicic','ADMIN');
